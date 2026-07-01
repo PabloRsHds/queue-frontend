c929c9353b0f76f8413651c82cf1a13fa8d32a1d
Pablo <pablorshds@gmail.com>
2026-06-30 22:53:21 -0300

    adicionando alterações na seção de config

--- src/app/components/config/config.component.ts
+++ src/app/components/config/config.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserStateService } from '../../services/states/user/user-state.service';
 
 @Component({
   selector: 'app-config',
@@ -7,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './config.component.css'
 })
 export class ConfigComponent {
+
+  // Injections
+  public userState = inject(UserStateService);
+
+  // Variables
+  public userLogged = this.userState.userLogged;
+
+
+  public getRoleDisplayName(role: string): string {
+    switch (role) {
+      case 'MANAGER': return 'Gerente';
+      case 'ATTENDANT': return 'Atendente';
+      case 'RECEPTION': return 'Recepcionista';
+      default: return 'Administrador';
+    }
+  }
 
 }
