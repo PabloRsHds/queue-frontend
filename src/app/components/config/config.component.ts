7a218ac3a154a00a24ac7470e7dc9448e7f4b63a
Pablo <pablorshds@gmail.com>
2026-07-01 22:44:14 -0300

    feat: adicionando melhorias no desing da seção de configuração

--- src/app/components/config/config.component.ts
+++ src/app/components/config/config.component.ts
@@ -1,19 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { UserStateService } from '../../services/states/user/user-state.service';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-config',
-  imports: [],
+  imports: [ CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './config.component.html',
   styleUrl: './config.component.css'
 })
 export class ConfigComponent {
 
   // Injections
+  public fb = inject(FormBuilder);
   public userState = inject(UserStateService);
 
   // Variables
   public userLogged = this.userState.userLogged;
+  public navItem:string = 'profile';
+  public showPassword: boolean = false;
+
+  // Form
+  public profileForm!: FormGroup;
+
+  constructor() {
+
+    const user = this.userLogged();
+
+    this.profileForm = this.fb.group({
+      name: [user?.name],
+      email: [user?.email],
+      phone: [user?.phone],
+      role: [this.getRoleDisplayName(user?.role ?? '')],
+    });
+  }
 
 
   public getRoleDisplayName(role: string): string {
@@ -23,6 +44,10 @@ export class ConfigComponent {
       case 'RECEPTION': return 'Recepcionista';
       default: return 'Administrador';
     }
+  }
+
+  public navItemChange(item: string) {
+    this.navItem = item;
   }
 
 }
